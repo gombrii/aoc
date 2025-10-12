93924f713c318df57242ebbe2dd8b55ccce70aa7
Simon Gombrii <simon@gombrii.se>
2025-10-12 20:30:09 +0200

    Add link to puzzle in generated solutin package

--- internal/genday.go
+++ internal/genday.go
@@ -7,7 +7,8 @@ import (
 	"github.com/gombrii/aoc/internal/gen"
 )
 
-const part1 = `package {{.Day}}
+const part1 = `// Package {{.DayName}} solves puzzle available on https://adventofcode.com/{{.Year}}/day/{{.Day}}
+package {{.DayName}}
 
 func Part1(data []byte) any {
 	//in := input.Lines(data)
@@ -15,7 +16,8 @@ func Part1(data []byte) any {
 	return "NOT IMPLEMENTED!"
 }`
 
-const part2 = `package {{.Day}}
+const part2 = `// Package {{.DayName}} solves puzzle available on https://adventofcode.com/{{.Year}}/day/{{.Day}}
+package {{.DayName}}
 
 func Part2(data []byte) any {
 	//in := input.Lines(data)
@@ -23,26 +25,29 @@ func Part2(data []byte) any {
 	return "NOT IMPLEMENTED!"
 }`
 
-const common = `package {{.Day}}`
+const common = `// Package {{.DayName}} solves puzzle available on https://adventofcode.com/{{.Year}}/day/{{.Day}}
+package {{.DayName}}`
 
 func GenDay(year, day string) error {
-	day = fmt.Sprintf("day%s", day)
+	dayName := fmt.Sprintf("day%s", day)
 
 	data := map[string]string{
-		"Day": day,
+		"Year":    year,
+		"Day":     day,
+		"DayName": dayName,
 	}
 
 	if err := gen.Files(data, map[string]string{
-		filepath.Join(year, "solutions", day, "part1.go"):  part1,
-		filepath.Join(year, "solutions", day, "part2.go"):  part2,
-		filepath.Join(year, "solutions", day, "common.go"): common,
-		filepath.Join(year, "input", day, "input.txt"):     "",
-		filepath.Join(year, "input", day, "test.txt"):      "",
+		filepath.Join(year, "solutions", dayName, "part1.go"):  part1,
+		filepath.Join(year, "solutions", dayName, "part2.go"):  part2,
+		filepath.Join(year, "solutions", dayName, "common.go"): common,
+		filepath.Join(year, "input", dayName, "input.txt"):     "",
+		filepath.Join(year, "input", dayName, "test.txt"):      "",
 	}); err != nil {
 		return fmt.Errorf("generating files: %v", err)
 	}
 
-	fmt.Printf("Initiated %s %s\n", day, year)
+	fmt.Printf("Initiated %s %s\n", dayName, year)
 
 	return nil
 }
